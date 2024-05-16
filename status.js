a38ea888185e00bb8c012924461409d2c04e0eeb
Nico Tarquini <45042953+nicosistemas@users.noreply.github.com>
2024-05-16 18:35:54 -0300

    Create status.js

---
+++ status.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Tu Página con JavaScript Externo</title>
+</head>
+<body>
+
+    <h1>¡Bienvenido a mi página con JavaScript externo!</h1>
+
+    <!-- Aquí incluimos el script externo -->
+    <script src="https://gosii1.statuspage.io/embed/script.js"></script>
+
+</body>
+</html>
